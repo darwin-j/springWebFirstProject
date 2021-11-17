e94f284c58582d3f0bdd71dbb59fbfea7d863109
Your Name <darwinmuthu450@gmail.com>
2021-11-17 19:17:44 +0530

    init

---
+++ src/main/java/first/spring/springWebFirstProject/SpringWebFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package first.spring.springWebFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/first/spring/springWebFirstProject/SpringWebFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package first.spring.springWebFirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
